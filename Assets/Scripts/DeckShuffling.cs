c6b9d197f49f7be9a601fab5274a72f9dc03ec49
gbarrettsnyder <gbarrettsnyder@smith.edu>
2016-02-05 09:33:02 -0500

    Revised DeckShuffling.cs

--- Assets/Scripts/DeckShuffling.cs
+++ Assets/Scripts/DeckShuffling.cs
@@ -7,8 +7,6 @@ public class DeckShuffling : MonoBehaviour {
 	public int [] shuffledDeck;
 	public int randomNum;
 
-	
-	// Use this for initialization
 	void Start () {
 		initialDeck = new int[52];
 		shuffledDeck = new int[52];
@@ -21,12 +19,14 @@ public class DeckShuffling : MonoBehaviour {
 		//loop to place all the numbers in the initial deck into a random spot in the shuffled deck
 		for (int j = 0; j <= 51; j++) {
 			bool cardPlaced = false;
-			//generated a random number between 0 and 51, checks if the value in the shuffled deck is 0, places the number from initial deck in the random index
 			//continues generating random number until an empty spot is found
 			while (cardPlaced == false) {
+                //generated a random number between 0 and 51
 				randomNum = Random.Range (0,52);
+                //checks if the value in the shuffled deck is 0
 				if(shuffledDeck[randomNum] == 0)
 				{
+                    //places the number from initial deck in the random index
 					shuffledDeck[randomNum] = initialDeck[j];
 					cardPlaced = true;
 				}
@@ -35,9 +35,5 @@ public class DeckShuffling : MonoBehaviour {
 		for (int k = 0; k<=51; k++) {
 			print (shuffledDeck[k]);
 		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
 	}
 }
