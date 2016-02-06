272b10770b6d40d45eaa177ff98e90d52afd1619
gbarrettsnyder <gbarrettsnyder@smith.edu>
2016-02-05 19:54:56 -0500

    Implemented UI Mockup

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+
+    string title;
+    string terrain;
+    string desc1;
+    string desc2;
+
+    int gold;
+    int salvage;
+
+    Sprite art;
+
+    public void Init(string title, string terrain, string desc1, string desc2, int gold, int salvage, Sprite art)
+    {
+        this.title = title;
+        this.terrain = terrain;
+        this.desc1 = desc1;
+        this.desc2 = desc2;
+        this.gold = gold;
+        this.salvage = salvage;
+        this.art = art;
+    }
+}
