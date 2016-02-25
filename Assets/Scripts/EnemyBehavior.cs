3573aebcc0973eca4a124de5b19c9d8ea6bd93fe
David Tuttle <dtuttle@umass.edu>
2016-02-24 21:38:02 -0500

    Minor optimization
    1. Fixed enemy conditional
    2. improved loop structure

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -21,27 +21,27 @@ public class EnemyBehavior : MonoBehaviour {
 
 	//Method to test playability of card in current state
 	private bool playable(CardObject card){
-		if((card.GetCardInfo().terrain == gameController.currTerrain.name) ){
-			//&& (card.GetCardInfo().daytime == gameController.currDayTime)){
-			return true;
+		if((card.GetCardInfo().terrain != gameController.currTerrain.name) ){
+			//&& (card.GetCardInfo().daytime == gameController.currDayTime)){        taken out for alpha
+			return false;
 		}
-		return false;
+		return true;
 	}
 
 
 	//TODO talk with design team to determine how a real player might value their cards
 	//TODO Change the behavior of enemy selction depending on what kind of enemy it is.
 
-	//Simple method for selcting card. Will increase complecity as design team completes more work
+	//Simple method for selcting card. Will increase complexity as design team completes more work
 	public CardObject selectCard(){
 		//Temp var to store highest valued playable card.
 		CardObject highestCard = null;
 		//Searches hand for desired card to play. Current criteria is for initial prototype only.
-		for(int i = 0; i < hand.Count; i++){
+		foreach(CardObject card in hand){
 			//Checks playability and relative value of card
-			if (playable(hand[i])&&(highestCard.GetCardInfo().aiValue < hand[i].GetCardInfo().aiValue)) {
+			if (playable(card)&&(highestCard.GetCardInfo().aiValue < card.GetCardInfo().aiValue)) {
 				//Sets temp variable to highest valued card;
-				highestCard = hand [i];
+				highestCard = card;
 			}
 		}
 		return highestCard;
