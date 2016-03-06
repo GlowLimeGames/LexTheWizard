911a0ee850a092cdf7360273ac7dda980aa07c74
David Tuttle <dtuttle@umass.edu>
2016-03-06 14:53:04 -0500

    Replaced Scripts Folder
    Took Scripts folder from pre-merge conflict branch

--- Assets/Scripts/DiscardPile.cs
+++ Assets/Scripts/DiscardPile.cs
@@ -46,7 +46,6 @@ public class DiscardPile : MonoBehaviour {
 		cardObject.GetOwner ().RemoveCardFromHand (cardObject);
         cardObject.gameObject.SetActive(false);
 		selectedCard = null;
-		CardGame.Instance.SetPositionFree (cardObject.GetHandPosition ()); // Set hand position as free
     }
 
     void OnCollisionEnter2D(Collision2D coll)
