0814995d4bf291ce5c6a16e8e311e4faa7f06d73
TimothyAdan <brassbgeek@gmail.com>
2016-03-16 14:13:40 -0400

    Audio Integration Tests
    Created a dummy game object to hold the ambiance script. Tested some
    code to make sure the right ambiance sound would trigger off of the
    right terrain index (based on the gamecontroller object).
    
    ambiance currently works. Might need improving. Also needs to be in a
    different place.

---
+++ Assets/Scripts/Audio/PlayAmbiance.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayAmbiance : MonoBehaviour {
+
+    string[] playAmbiance = { "Any", "PlayForestAmbiance", "PlayHillAmbiance", "PlaySwampAmbiance", "PlayCaveAmbiance" };
+    private int currPlaying;
+     private int gameController = GameController.gameController.currTerrainIndex;
+
+
+    void Update()
+    {
+        gameController = GameController.gameController.currTerrainIndex;
+        if (gameController != currPlaying)
+        {
+            currPlaying = gameController;
+            EventController.Event(playAmbiance[currPlaying]);
+        }
+        else
+        {
+            
+        }
+    }
+	
+}
