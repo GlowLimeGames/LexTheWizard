71f30b183a83360e55f8f1ec21263be1ba08abff
Leticia Rossi <lrossidossantos18@amherst.edu>
2016-04-13 20:00:16 -0400

    Initial options scene try

---
+++ Assets/Scripts/OptionsManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsManager : MonoBehaviour {
+
+	bool musicToggle = false;
+	bool sfxToggle = false;
+	bool ambianceToggle = false;
+
+	void Update () {
+		//SettingsUtil.ToggleMusicMuted (musicToggle);
+
+	}
+
+
+	public void ToggleMusic(){
+		musicToggle = !musicToggle;
+		SettingsUtil.ToggleMusicMuted (musicToggle);
+	}
+
+	public void ToggleFX(){
+		sfxToggle = !sfxToggle;
+		SettingsUtil.ToggleFXMuted (sfxToggle);
+	}
+
+	public void ToggleAmbiance(){
+		ambianceToggle = !ambianceToggle;
+		SettingsUtil.ToggleVOMuted (ambianceToggle);
+	}
+}
