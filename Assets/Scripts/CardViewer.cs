9ae72f2cb4c7f77cc91a30810723c0244ae8f951
SiennaC <sc16@hampshire.edu>
2016-09-29 20:27:27 -0400

    Skeleton UI

---
+++ Assets/Scripts/CardViewer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CardViewer : MonoBehaviour {
+    public Text cardName;
+    public Text description;
+    public Image image;
+    
+    public int index = -1;
+    private Card card;
+
+    public int Index {
+        get { return index; }
+        set { index = value; }
+    }
+
+    public Card Card {
+        get { return card; }
+        set {
+            card = value;
+
+            if (card != null) {
+                Show(true);
+                UpdateCard();
+            }
+            else {
+                Show(false);
+            }
+        }
+    }
+
+
+    void Start () {
+        Show(card != null);
+    }
+	
+
+	private void UpdateCard () {
+        cardName.text = card.Name;
+        description.text = card.Description;
+        image.sprite = card.Image;
+	}
+
+
+    public void Show (bool show) {
+        for (int i = 0; i < transform.childCount; i++) {
+            transform.GetChild(i).gameObject.SetActive(show);
+        }
+    }
+}
