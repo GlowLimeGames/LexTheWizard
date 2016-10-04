d092f79ca7350e4ac900680d515b534e2a56060b
Timothy Ng <tdng@umass.edu>
2016-10-03 23:34:42 -0400

    Fixed anchors for UI

--- Assets/Scripts/AIPlayState.cs
+++ Assets/Scripts/AIPlayState.cs
@@ -5,18 +5,9 @@ using UnityEngine.UI;
 
 public class AIPlayState : MonoBehaviour{
 
-    public Text eventText;
-    private float timeLeft = 2f;
-
     void Update()
     {
-        eventText.text = "AI played a card";
-        timeLeft -= Time.deltaTime;
-        if (timeLeft <= 0)
-        {
-            timeLeft = 2f;
-            GameController.INSTANCE.NextState();
-        }
+        GameController.INSTANCE.NextState();
     }
 
 }
--- Assets/Scripts/PlayCardState.cs
+++ Assets/Scripts/PlayCardState.cs
@@ -11,47 +11,7 @@ public class PlayCardState : MonoBehaviour{
 
     public void NextButton()
     {
-        GameController.INSTANCE.Card1 = null;
-        GameController.INSTANCE.Card2 = null;
-        GameController.INSTANCE.Card3 = null;
+        
         GameController.INSTANCE.NextState();
-    }
-
-    public void PlayCard(int card)
-    {
-        if(card == 1 && GameController.INSTANCE.Card1 != null)
-        {
-            GameController.INSTANCE.Card1 = null;
-            GameController.INSTANCE.Points++;
-        }
-        else if(card == 2 && GameController.INSTANCE.Card2 != null)
-        {
-            GameController.INSTANCE.Card2 = null;
-            GameController.INSTANCE.Points++;
-        }
-        else if(card == 3 && GameController.INSTANCE.Card3 != null)
-        {
-            GameController.INSTANCE.Card3 = null;
-            GameController.INSTANCE.Points++;
-        }
-    }
-
-    public void ScrapCard(int card)
-    {
-        if (card == 1 && GameController.INSTANCE.Card1 != null)
-        {
-            GameController.INSTANCE.Card1 = null;
-            GameController.INSTANCE.Mana++;
-        }
-        else if (card == 2 && GameController.INSTANCE.Card2 != null)
-        {
-            GameController.INSTANCE.Card2 = null;
-            GameController.INSTANCE.Mana++;
-        }
-        else if (card == 3 && GameController.INSTANCE.Card3 != null)
-        {
-            GameController.INSTANCE.Card3 = null;
-            GameController.INSTANCE.Mana++;
-        }
     }
 }
