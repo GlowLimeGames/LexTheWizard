cba87461092cb7425d5902b6e19d36de0a68da7a
Isaiah Mann <contact@isaiahmann.com>
2016-12-20 23:59:38 -0500

    Adding some comments to update background script

--- Assets/Scripts/UpdateBackgroundState.cs
+++ Assets/Scripts/UpdateBackgroundState.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿/*
+ * Author(s): Tim Ng, Noah Cohen, Sienna Cornish, Isaiah Mann
+ * Description: Updates the background display of the game in accordance with the terrain type and time of day
+ */
+
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
@@ -6,6 +11,12 @@ public class UpdateBackgroundState : MonoBehaviour {
     public static UpdateBackgroundState INSTANCE;
     public Image background;
 
+	// Time of day is arranged by the GameController.DayTime enum 
+	/* Enum Value --> Sprite Key:
+	 * Dawn --> Morning
+	 * Dusk --> Afternoon
+	 * Night --> Night
+	 */
 	[SerializeField]
 	Sprite[] caveBackgrounds;
 	[SerializeField]
@@ -15,6 +26,7 @@ public class UpdateBackgroundState : MonoBehaviour {
 	[SerializeField]
 	Sprite[] swampBackgrounds;
 
+	// Stores all the terrain types in a jagged array
 	Sprite[][] terrainBackgrounds;
 
 	void Awake() {
@@ -38,17 +50,21 @@ public class UpdateBackgroundState : MonoBehaviour {
 		Fungus.Flowchart.BroadcastFungusMessage("UpdateBackgroundStateStart");
     }
 
+	// Update is only called once per turn (this MonoBehaviour is disabled at all other times)
 	void Update () {
 		UpdateBackground();
+		// Necessary for game logic
 		GameController.INSTANCE.NextState();
 	}
 
     public void UpdateBackground () {
+		// Casting enum values to integers for direct indexing into the array
 		int terrainIndex = (int) GameController.INSTANCE.currentTerrain;
 		int timeOfDayIndex = (int) GameController.INSTANCE.currentDayTime;
 		try {
 			background.sprite = terrainBackgrounds[terrainIndex][timeOfDayIndex];
 		} catch (System.Exception e) {
+			// Handle an errors, presumabmly accounting for index out of range exceptions
 			Debug.LogErrorFormat("Error: {0}, [{1}, {2}] is not a valid index", e, terrainIndex, timeOfDayIndex);
 		}
 	}
