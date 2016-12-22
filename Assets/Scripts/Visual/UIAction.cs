4d29fe35cb5a9b40b7b705e5a77e86f5d2041bb5
Isaiah Mann <contact@isaiahmann.com>
2016-12-21 21:57:35 -0500

    Fixing bugs with UI event system

--- Assets/Scripts/Visual/UIAction.cs
+++ Assets/Scripts/Visual/UIAction.cs
@@ -1,32 +1,8 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class UIAction : ScriptableObject {
 	public virtual void Execute (UIElement target){
 		Debug.LogFormat("Executing UI action on {0}", target);
 	}
 }
-
-
-[CreateAssetMenuAttribute(fileName = "Show", menuName = "UIEvent/Show", order = 0)]
-public class UIShowAction : UIAction {
-	public override void Execute (UIElement target){
-		target.Show();
-	}
-}
-
-
-[CreateAssetMenuAttribute(fileName = "Hide", menuName = "UIEvent/Hide", order = 1)]
-public class UIHideAction : UIAction {
-	public override void Execute (UIElement target){
-		target.Hide();	
-	}
-}
-
-
-[CreateAssetMenuAttribute(fileName = "RandomSprite", menuName = "UIEvent/RandomSprite", order = 2)]
-public class UIRandomSpriteAction : UIAction {
-	public override void Execute (UIElement target){
-		target.RandomSprite();
-	}
-}
+	
---
+++ Assets/Scripts/Visual/UIHideAction.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenuAttribute(fileName = "Hide", menuName = "UIEvent/Hide", order = 1)]
+public class UIHideAction : UIAction {
+	public override void Execute (UIElement target){
+		target.Hide();	
+	}
+}
